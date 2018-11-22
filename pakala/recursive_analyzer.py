c2c43fc76b5d93fe70a332752075490444457058
palkeo <contact@palkeo.com>
2018-11-23 00:57:49 +0100

    Add temporary hack because the env can be identical.

--- pakala/recursive_analyzer.py
+++ pakala/recursive_analyzer.py
@@ -189,8 +189,11 @@ class RecursiveAnalyzer(analyzer.BaseAnalyzer):
                 logger.warning('path: %s', path)
                 breakpoint()
 
-            new_composite_states = self._append_state(
-                    initial_composite_state, path[-1])
+            try:
+                new_composite_states = self._append_state(
+                        initial_composite_state, path[-1])
+            except AssertionError:
+                continue
 
             for composite_state in new_composite_states:
                 if self._search_path(composite_state, path) is not None:
