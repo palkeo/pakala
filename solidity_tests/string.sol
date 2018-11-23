4bcc3d83a9d5cb5244b32b361fcd4cadf5c60e93
palkeo <contact@palkeo.com>
2018-11-23 01:52:18 +0100

    Add solidity tests.

--- solidity_tests/string.sol
+++ solidity_tests/string.sol
@@ -1,6 +1,6 @@
 contract String {
   function passwordSend(bytes32 s) public payable {
     require(s == "12345678901234567890magic");
-    msg.sender.send(this.balance / 10);
+    msg.sender.send(this.balance / 5 - (0.1 ether));
   }
 }
