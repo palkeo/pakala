5461ac1475fd4d60dcbe7865ac47a94c9749cc33
palkeo <contact@palkeo.com>
2018-12-26 15:20:47 +0100

    Reformatting.

--- pakala.py
+++ pakala.py
@@ -49,7 +49,7 @@ def ethWeiAmount(arg):
 
 
 def addressOrStdin(s):
-    if s == '-':
+    if s == "-":
         return s
     if not re.match(r"^0x([0-9a-fA-F]){40}$", s):
         raise argparse.ArgumentError("Invalid address.")
