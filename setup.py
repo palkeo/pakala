d310332b27a7d52271117d311a940cfc86bcedd9
palkeo <contact@palkeo.com>
2019-03-02 15:11:26 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pakala',
-    version='1.0.11',
+    version='1.0.12',
 
     description='An EVM symbolic execution tool and vulnerability scanner',
 
