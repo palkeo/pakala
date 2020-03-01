fce61037e511321cfd97b8adf88f5d3b74781099
palkeo <contact@palkeo.com>
2020-03-01 21:14:30 +1300

    Reformat setup.py with black.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -7,48 +8,35 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt')) as f:
+with open(path.join(here, "requirements.txt")) as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='pakala',
-    version='1.1.10',
-
-    description='An EVM symbolic execution tool and vulnerability scanner',
-
+    name="pakala",
+    version="1.1.10",
+    description="An EVM symbolic execution tool and vulnerability scanner",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    url='https://github.com/palkeo/pakala',
-
-    author='palkeo',
-
-    author_email='ethereum@palkeo.com',
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/palkeo/pakala",
+    author="palkeo",
+    author_email="ethereum@palkeo.com",
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
+        "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-
+        "Intended Audience :: Developers",
         # Pick your license as you wish
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-
-        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
     ],
-
-    keywords='ethereum evm symbolic execution vulnerability scanner',
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
+    keywords="ethereum evm symbolic execution vulnerability scanner",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
     install_requires=requirements,
-
-    entry_points={'console_scripts': ['pakala=pakala.cli:main']},
+    entry_points={"console_scripts": ["pakala=pakala.cli:main"]},
 )
